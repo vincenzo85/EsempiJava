5830268c74ab5658fa39e4321f4b28d692f894ce
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-01-22 22:14:26 +0100

    Aggiornato il progetto
    aggiornato partecipante.java e leggimi

--- src/it/esempijava/crm/model/Partecipante.java
+++ src/it/esempijava/crm/model/Partecipante.java
@@ -1,5 +1,19 @@
 package it.esempijava.crm.model;
 
 public class Partecipante {
+	
+	/**
+	 *  variabile di instanza, caratterizzano lo stato della classe ;
+	 */
+	
+	private static int counter = 0;
+	private int idx;
+	
+	private String nome;
+	private String email;
+	private int eta;
+	private String cognome;
+	
+	
 
 }
