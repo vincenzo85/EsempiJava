2a4dea2fab5bfb9b50ee96380f6d5516ed77a6c3
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-01-22 21:41:16 +0100

    esempio stampa
    primo esempio stampa

--- src/it/esempijava/Application.java
+++ src/it/esempijava/Application.java
@@ -4,7 +4,26 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+		/* Cambiando il right value di Esempio lo switch case attiva il relativo metodo dell'esempio */
+		
+		int Esempio = EsempiLista.ESEMPIO_STAMPA;
+		
+		
+		switch (Esempio) {
+		case EsempiLista.ESEMPIO_STAMPA:
+			
+			stampa();
+						
+			break;
 
+		
+		}
+		
+
+	}
+	
+	public static void stampa() {
+		System.out.println("Testo stampato su console");
 	}
 
 }
---
+++ src/it/esempijava/EsempiLista.java
@@ -0,0 +1,9 @@
+package it.esempijava;
+
+public class EsempiLista {
+	
+	public static final int ESEMPIO_CLASSE = 0;
+	public static final int ESEMPIO_STAMPA = 1;
+	
+
+}
