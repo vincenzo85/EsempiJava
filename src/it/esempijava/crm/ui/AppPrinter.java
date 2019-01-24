147c70423e9f75dd4ae5949a97635267fbddbbfb
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-01-24 15:32:35 +0100

    ho ridotto tutto il codice
    ho ridotto il codice del main, centralizzando tutto nel single point of failiure

--- src/it/esempijava/crm/ui/AppPrinter.java
+++ src/it/esempijava/crm/ui/AppPrinter.java
@@ -2,6 +2,7 @@ package it.esempijava.crm.ui;
 
 import it.esempi.interfacce.ConsolePrinter;
 import it.esempi.interfacce.Printer;
+import it.esempi.interfacce.PrinterBuilder;
 import it.esempi.interfacce.TxtPrinter;
 import it.esempijava.crm.model.Utente;
 import it.esempijava.repo.Repository;
@@ -15,33 +16,8 @@ public class AppPrinter {
 		new Repository();
 		Utente[] data = Repository.getUtentiTable();
 		
-		Printer printer = null;
-		
-		
-		
-		switch(printerType) {
-		
-		case 0:
-		//	ConsolePrinter cp = new ConsolePrinter();
-			printer = new ConsolePrinter();
-			/* Reference di tipo printer con un costruttore ConsolePrinter   */
-		
-			
-			break;
-			
-		case 1:
-			
-			printer = new TxtPrinter();
-			
-			break;
-			
-		default: 
-			System.out.println("Formato di stampa non supportato");
-			
-		}
-		// non mi interessa sapere su quale printer lo sto chiamando, gli interessa selezionare il printer giusto in base al tipo 
+		Printer printer = PrinterBuilder.getPrinter(printerType);
 		printer.print(data);
-		
 		
 
 	}
