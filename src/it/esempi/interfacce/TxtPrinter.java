78b74fc5478ac91c72a52deefeab7d20e50e8114
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-01-24 15:17:56 +0100

    Due interfacce instaziate

--- src/it/esempi/interfacce/TxtPrinter.java
+++ src/it/esempi/interfacce/TxtPrinter.java
@@ -2,7 +2,7 @@ package it.esempi.interfacce;
 
 import it.esempijava.crm.model.Utente;
 
-public class TxtPrinter {
+public class TxtPrinter implements Printer {
 	
 	public void print(Utente[] data) {
 		
