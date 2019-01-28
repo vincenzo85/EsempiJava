fcf52e499a459a91c696d473b52e9bf6ec89a78e
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-01-28 12:11:07 +0100

    Collection list

---
+++ src/it/esempi/collection/Collection.java
@@ -0,0 +1,62 @@
+package it.esempi.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Collection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		lista();
+		listaStringhe();
+		
+		
+	}
+	
+	public static List<Integer> lista() {
+		
+		List listaUtenti = new ArrayList();
+		
+		// "array list implements list";
+		
+		// " scatola di oggetti di tipo diverso 
+		
+		System.out.println(listaUtenti.add(new Integer(10)));
+		listaUtenti.add(new String("ciao"));
+		
+				
+		return listaUtenti;
+		
+	}
+	
+	public static List<String> listaStringhe() {
+		
+		
+		List<String> listaUtenti = new ArrayList<String>();
+		
+		// "array list implements list";
+		
+		// " scatola di oggetti di tipo diverso 
+	
+		listaUtenti.add(new String("altro"));
+		listaUtenti.add(new String("ciao"));
+		System.out.println("test");
+		Iterator<String> iterator = listaUtenti.iterator();
+		
+		while(iterator.hasNext()) {
+			System.out.println((String) iterator.toString());
+			iterator.next();
+			}
+		
+		return  listaUtenti;
+		
+		
+		
+		
+	}
+	
+	
+
+}
